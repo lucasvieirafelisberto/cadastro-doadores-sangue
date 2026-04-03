42f79e9a737a6392f3fa265ed8c8fe7b6cf33f02
lucasvieirafelisberto <lucasvieira.trabalhos@gmail.com>
2026-04-03 19:41:05 -0300

    feat: inicializa projeto de cadastro de doadores de sangue

---
+++ main.py
@@ -0,0 +1,21 @@
+#Cadastro da ficha do doador
+print('=' * 50)
+print("Bem-vindo ao Cadastro de doadores de sangue")
+nome = str(input("Por favor informe o seu nome completo: "))
+peso = float(input("Por favor informe o seu peso: "))
+altura = int(input("Por favor informe sua altura em cm: "))
+ano_nascimento = int(input("Por favor informe seu ano de nascimento: "))
+
+#Regras
+idade = 2026 - ano_nascimento #Colocar ano atual.
+tem_peso_minimo = peso >= 50
+tem_idade_minima = idade >= 16 and idade <= 69
+
+#Saída dos valores informados:
+print(f"NOME: {nome}")
+print(f"PESO: {peso}kg")
+print(f"ALTURA: {altura}cm")
+print(f"IDADE: {idade} anos")
+print(f"TEM PESO MINIMO PARA DOAR? {tem_peso_minimo}")
+print(f"ESTÁ NA IDADE CORRETA PARA DOAR ENTRE 16 A 69 ANOS? {tem_idade_minima}")
+print('=' * 50)
